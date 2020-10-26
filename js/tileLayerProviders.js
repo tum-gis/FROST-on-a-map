ac669343a3df999281c57d882e45a70e7ecea70d
Bruno Willenborg <b.willenborg@tum.de>
2020-10-26 18:17:53 +0100

    minor fixes

--- js/tileLayerProviders.js
+++ js/tileLayerProviders.js
@@ -1,4 +1,4 @@
-  // Mapbox background
+  // Mapbox tile layer
   var mapboxToken = '';
   var tileLayer = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -7,10 +7,10 @@
     tileSize: 512,
     zoomOffset: -1,
     accessToken: mapboxToken
-  }).addTo(mymap);
+  }).addTo(map);
 
 
-  // Open Topo Map background
+  // Open Topo Map tile layer
   var tileLayer = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors | <a href="http://viewfinderpanoramas.org/">SRTM</a> | Map style &copy; <a href="https://opentopomap.org/">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>)',
     maxZoom: 18,
@@ -18,10 +18,10 @@
     subdomains: 'abc',
     zoomOffset: -1,
     tileSize: 512
-  }).addTo(mymap);
+  }).addTo(map);
 
 
-  // OSM background map
+  // OSM tile layer
   var tileLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     maxZoom: 19,
@@ -29,13 +29,13 @@
     subdomains: 'abc',
     zoomOffset: -1,
     tileSize: 512
-  }).addTo(mymap);
+  }).addTo(map);
 
-  // Open Topo Map background
+  // Thunderforest tile layer
   var thunderforestAPIKey = '';
   var tileLayer = L.tileLayer('https://{s}.tile.thunderforest.com/{style}/{z}/{x}/{y}.png?apikey={apikey}', {
     attribution: '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     maxZoom: 22,
     style: 'neighbourhood', // cycle | transport | transport-dark | landscape | outdoors | spinal-map | pioneer | mobile-atlas | neighbourhood
     apikey: thunderforestAPIKey
-  }).addTo(mymap);
+  }).addTo(map);
